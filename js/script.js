19909bf5920b18310597050b9fb16f945a11c5a4
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2023-01-11 22:51:08 +0000

    trying to get an image to show up 😅

--- js/script.js
+++ js/script.js
@@ -25,6 +25,8 @@ const getCat = async (URLAddress) => {
     const result = await fetch(URLAddress)
     const jsonData = await result.json()
     console.log(jsonData[0].url)
+    document.getElementById("api-image").innerHTML = '<img src"https://cdn2.thecatapi.com/images/' + '@2x.png" alt="API image" class="center" width= 20% height 20%' +
+      ">"
 
 
 
